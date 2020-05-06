df0bf2aaa60b7609821c99a5a50696e618af3a4a
krishanthini2998 <63590498+krishanthini2998@users.noreply.github.com>
2020-05-06 20:32:38 +0530

    newcommit

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -4,11 +4,11 @@ class QuizQuestion extends Component{
  render(){
    <main>
         <section>
-          <p>// instruction text goes here</p>
+          <p></p>
         </section>
         <section className="buttons">
           <ul>
-            //quiz question button logic goes here
+            <li>{this.props.quiz_question.answer_options[0]}</li>
           </ul>
         </section>
       </main>
